0baabd8c74b998a3af9f376fb755b0f0b3103be0
ZiadAhmed92 <za693387@gmail.com>
2023-12-07 14:10:13 +0200

    test

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,8 @@
-import Layout from '../Components/Layout'
-import '../styles/globals.css'
-import '../../node_modules/bootstrap/dist/css/bootstrap.min.css'
-// import '../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
-import '../../node_modules/@fortawesome/fontawesome-free/css/all.min.css'
+import Layout from "../Components/Layout"
+import "../styles/globals.css"
+import "../../node_modules/bootstrap/dist/css/bootstrap.min.css"
+// import "../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"
+import "../../node_modules/@fortawesome/fontawesome-free/css/all.min.css"
 
 export default function App({ Component, pageProps }) {
   return <Layout>
--- src/pages/register.js
+++ src/pages/register.js
@@ -1,19 +1,19 @@
-import React from 'react'
+import React from "react"
 
 const Register = () => {
   return (
-    <div className='mt-5 message-area'>
-      <div className='icon-register'>
+    <div className="mt-5 message-area">
+      <div className="icon-register">
         <i className="fa-regular fa-pen-to-square"></i>
 
       </div>
-      <h2 className='text-info text-reg'>Register</h2>
-      <form className='register py-5'>
+      <h2 className="text-info text-reg">Register</h2>
+      <form className="register py-5">
         
-          <input type='text' placeholder='Enter Your Name' className='input-name' name='Name' />
-          <input type='text' placeholder='Enter Your Name' className='input-name' name='Name' />
-          <input type='text' placeholder='Enter Your Name' className='input-name' name='Name' />
-          <div className='mt-1'>
+          <input type="text" placeholder="Enter Your Name" className="input-name" name="Name" />
+          <input type="text" placeholder="Enter Your Name" className="input-name" name="Name" />
+          <input type="text" placeholder="Enter Your Name" className="input-name" name="Name" />
+          <div className="mt-1">
             <button className=" btn-register mt-1"> Register </button>
             <button className=" btn-register mt-3"> I Have An Account </button>
           </div>
